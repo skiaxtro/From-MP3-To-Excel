08d4b714c57c0c52e7971994dc2c21e350062395
skiaxtro <63364952+skiaxtro@users.noreply.github.com>
2022-04-16 23:04:44 +0300

    Add files via upload

---
+++ README.md
@@ -0,0 +1,28 @@
+
+# From MP3 To Excel [![MIT License](https://img.shields.io/apm/l/atomic-design-ui.svg?)](https://github.com/tterb/atomic-design-ui/blob/master/LICENSEs)
+This application takes as input all the audio **MP3** files you have in a folder, isolates 
+the artist's name and song title, and exports them to an **Excel** file.
+
+For example, the following folder:
+
+![Before](https://i.postimg.cc/pTyNGDQY/before.png)
+
+Will be converted to the following result:
+
+![After](https://i.postimg.cc/Twymc3wN/after.png)
+
+
+
+
+## Instructions
+- Place mp3 files to a folder.
+- Run the [**`from_mp3_to_excel.py`**](from_mp3_to_excel.py) file.
+- Type the path of your folder. **Beware**, Type it according your **OS**. For example, 
+  at windows you must use double “\\\” ( C:\\\\..\\\\\..\\\\).
+- Type the name of Excel file.
+## License
+My code is released under [MIT License](LICENSE).
+## Author
+
+- [@Stylianos Eleftheriadis](https://www.linkedin.com/in/stylianos-eleftheriadisdis/)
+
---
+++ from_mp3_to_excel.py
@@ -0,0 +1,54 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Apr 16 17:43:37 2022
+
+@author: Stylianos Eleftheriadis
+"""
+import os
+from pandas import DataFrame
+from mutagen.easyid3 import EasyID3
+
+#path = "C:\\Users\\skiax\\OneDrive\\Desktop\\diva" # No input Method
+
+# Input Method
+path = input("Please enter a valid folder path\nFor example (Windows): C:\\\..\\\..\\\..\\\<name of folder>\n\n")
+print(f'\n\nYou entered:\n{path}')
+os.chdir(path)
+
+# Set Excel's name from keyboard
+name = input("Set the Excel's name:\n\n")
+print(f'\n\nYou entered:\n{name}')
+
+# Read every file in the folder
+def read_folder(directory):
+    
+    files_name = []  
+
+    for root, directories, files in os.walk(directory):
+        for filename in files:
+            filepath = os.path.join(filename)
+            files_name.append(filepath)  
+
+    return files_name  
+
+folder = read_folder(path)
+length = len(folder)
+
+tick = [""] * length  # I need it to "tick" every song I played. Remove it if you dont want it!
+number = list(range(1, length+1))
+title = []
+artist = []
+
+for i in range(length):
+    audio = EasyID3(folder[i])
+    
+    title.append(audio['title'][0])
+    artist.append(audio['artist'][0])
+
+# Create data frame
+df = DataFrame({'N': tick, '#': number,'SONG': title,'ARTIST': artist})
+
+# Export data frame to excel
+df.to_excel(name+'.xlsx', sheet_name=name, index=False)
+
+print("\n\nThe Excel file was added to the folder's path!\n\n\n")
